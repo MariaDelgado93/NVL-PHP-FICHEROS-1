37b172be1afbeb65baa006c4247e758ac0ea27c4
MariaDelgado93 <56487785+MariaDelgado93@users.noreply.github.com>
2019-11-27 22:58:55 +0100

    Update fichero.php

--- fichero.php
+++ fichero.php
@@ -1,6 +1,6 @@
 <?php
 
-$fd = fopen('https://gist.githubusercontent.com/jsdario/6d6c69398cb0c73111e49f1218960f79/raw/8d4fc4548d437e2a7203a5aeeace5477f598827d/el_quijote.txt','r');
+$fd = fopen('el_quijote.txt','r');
 
 while (($contenido = fgets($fd)) !== false){
   echo "La palabra molino aparece" . substr_count($contenido, "molino") . "veces" . "\n";
